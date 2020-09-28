720d5bc8202dd67b7f27280751e3c5e9855ce1fb
Caroline E <caroline.esquivel01@gmail.com>
2020-09-28 16:59:41 -0500

    java continued exericise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,5 +4,47 @@ public class HelloWorld {
         System.out.print("Hello,");
         System.out.print(" Bio!");
 //        System.out.println();
+
+
+//        int myFavoriteNumber = 5;
+//        System.out.println(myFavoriteNumber);
+//
+////        char myString = 3.14159;
+////        System.out.println(myString);
+//
+//        float myNumber = 3.14F;
+//        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//
+////        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//
+//        int three = (int) "three";
+//        System.out.println(three);
+
+//        int x = 4;
+//        x += 5;
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.println(x);
+//        System.out.println(y);
+
+        int x = 10;
+        int y = 2;
+        x /=  y;
+        y -= x;
+        System.out.println(x);
+        System.out.println(y);
     }
 }
