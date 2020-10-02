ef040fe0a876d64f504e39007c70a45a5bd5769e
Caroline E <caroline.esquivel01@gmail.com>
2020-10-02 11:38:29 -0500

    complete

---
+++ src/MethodsExercises.java
@@ -0,0 +1,11 @@
+public class MethodsExercises {
+    public static void main(String[] args){
+// 1. Create four separate methods. Each will perform an arithmetic operation:
+//a.
+//Addition
+//Subtraction
+//Multiplication
+//Division
+
+    }
+}
