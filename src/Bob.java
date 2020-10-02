2848511c22df8b4599e44288250589486fd0901e
Caroline E <caroline.esquivel01@gmail.com>
2020-10-02 13:30:20 -0500

    talking with Bob

--- src/Bob.java
+++ src/Bob.java
@@ -1,25 +1,25 @@
 import java.util.Scanner;
 
 public class Bob {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         String question = ("Sure.");
-        String exclamation= ("Whoa, chill out!");
+        String exclamation = ("Whoa, chill out!");
         String empty = ("Fine. Be that way!");
         String whatever = ("Whatever...");
-        for (int i = 0; i <= 4; i++) {
-            Scanner questionForBob = new Scanner(System.in);
-            System.out.println("What would you like to say to Bob?: ");
-            String userInput = questionForBob.nextLine();
+
+        Scanner questionForBob = new Scanner(System.in);
+        System.out.println("What would you like to say to Bob?: ");
+        String userInput = questionForBob.nextLine().trim();
 
             if (userInput.endsWith("?")) {
                 System.out.println(question);
             } else if (userInput.endsWith("!")) {
-                System.out.println( exclamation);
-            } else if (userInput.length() == 0) {
+                System.out.println(exclamation);
+            } else if (userInput.length() == 0) { //<--or (userInput.equals(""))
                 System.out.println(empty);
             } else {
                 System.out.println(whatever);
-            }
+
         }
     }
 }
--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,9 +1,13 @@
+import java.sql.SQLOutput;
+
 public class StringExercise {
     public static void main(String[] args) {
         String song = ("We don't need no education We don't need no thought control");
         System.out.println(song.substring(0, 26) + "\n" + (song.substring(27, 59)));
+//        or
+        System.out.println("We don't need no education.\nWe don't need no thought control");
 
-        String inside = ("Check \"this\" out!, \"s\" inside of \"s!");
+        String inside = ("Check \"this\" out!, \"s inside of \"s!");
         System.out.println(inside);
 
         String windows = ("In windows, the main drive is usually C:\\");
